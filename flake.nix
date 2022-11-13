f180db2a89ba061dbd3b543a6dd336fe42962bf1
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2022-11-13 12:35:54 +0100

    Change overlay to add emacsPackage rather than regular nix package

--- flake.nix
+++ flake.nix
@@ -2,24 +2,29 @@
   description = "syncorate.el - An emacs-interface for syncorate.com";
 
   outputs = { self }: {
-    overlays.default = final: prev: {
-      syncorate-el = final.emacsPackages.trivialBuild rec {
-        pname = "syncorate.el";
-        version = "1.0.0";
+    overlays.emacs = final: prev:
+      let
+        syncorate-el = final.emacsPackages.trivialBuild rec {
+          pname = "syncorate.el";
+          version = "1.0.0";
 
-        src = ./.;
+          src = ./.;
 
-        buildInputs = [ final.emacs ];
+          buildInputs = [ final.emacs ];
 
-        meta = with final.lib; {
-          homepage = "https://github.com/sebastiant/syncorate.el";
-          description = "Emacs interface for Syncorate";
-          longDescription = ''
-            Display Focus status in mode-line and start Focus sessions from Emacs.
-          '';
-          inherit (final.emacs.meta) platforms;
+          meta = with final.lib; {
+            homepage = "https://github.com/sebastiant/syncorate.el";
+            description = "Emacs interface for Syncorate";
+            longDescription = ''
+              Display Focus status in mode-line and start Focus sessions from Emacs.
+            '';
+            inherit (final.emacs.meta) platforms;
+          };
         };
+        overrides = efinal: eprev: { inherit syncorate-el; };
+      in {
+        emacsPackagesFor = emacs:
+          (prev.emacsPackagesFor emacs).overrideScope' overrides;
       };
-    };
   };
 }
